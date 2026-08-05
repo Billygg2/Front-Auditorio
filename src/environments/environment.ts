abe08d6f9ba5c1fbe6de794586e38d4a8e416b51
Billygg2 <billsalme@gmail.com>
2026-08-04 20:02:55 -0500

    Configuracion Back de render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // ENTORNO DE DESARROLLO: define la dirección base utilizada para comunicarse con el backend.
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'https://back-end-auditorio.onrender.com',
   appName: 'Sistema de Reservas de Auditorio UNIBE',
   version: '1.0.0'
 };
